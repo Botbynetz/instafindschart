bb3f510eb8b07683d40898ca4190cac5495e192b
Botbynetz <104172778+Botbynetz@users.noreply.github.com>
2026-03-04 23:03:30 +0700

    Change Supabase anon key in configuration
    Updated the Supabase anon key to a new value.

--- supabase-config.js
+++ supabase-config.js
@@ -3,7 +3,7 @@
 // ========================
 if (typeof window.supabaseInitialized === 'undefined') {
     const SUPABASE_URL = 'https://uetkbvwcqipwrgjbvlwq.supabase.co';
-    const SUPABASE_ANON_KEY = 'sb_publishable_Nly81h1k3aBGzBk5jiiMfw_p7QWFOu7';
+    const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVldGtidndjcWlwd3JnamJ2bHdxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI2MjU3MDgsImV4cCI6MjA4ODIwMTcwOH0.TLVOrBbN9JmU8uC-Lw-jRJXaplUf1Ltkf6CRaNmDN2k;
 
     // Simpan referensi library sebelum di-overwrite
     const supabaseLib = window.supabase;
